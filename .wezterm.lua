11e6e1544c35ef695f64ef9cf3c713cac591730a
Charles Qiu <Thinking.QMonkey@GMail.com>
2026-08-06 17:15:12 +0800

    Create .wezterm.lua

---
+++ .wezterm.lua
@@ -0,0 +1,36 @@
+local wezterm = require 'wezterm'
+local config = {
+  color_scheme = 'Monokai',
+  window_background_opacity = 0.9,
+  macOS_window_background_blur = 0,
+  text_background_opacity = 0.9,
+  window_decorations = 'NONE',
+}
+
+wezterm.on('format-tab-title', function(tab)
+  local pane = tab.active_pane
+  local title = pane.title
+  return wezterm.format {
+    { Text = title },
+  }
+end)
+
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+  config.default_domain = 'WSL:Ubuntu'
+end
+
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
+config.keys = {
+  { key = 'R', mods = 'CTRL|SHIFT', action = wezterm.action_callback(function(win, pane)
+    win:toast_notification('wezterm', 'Config reloaded')
+    wezterm.reload_configuration()
+  end) },
+  { key = 'W', mods = 'CTRL|SHIFT', action = wezterm.action.CloseCurrentTab { confirm = true } },
+  { key = 'Q', mods = 'CTRL|SHIFT', action = wezterm.action.QuitApplication },
+}
+
+return config
