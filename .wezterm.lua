6dae4e47059dd69cbb06d5b741f7ca6eaf1d2424
Charles Qiu <Thinking.QMonkey@GMail.com>
2026-08-06 23:58:00 +0800

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require 'wezterm'
+
 local config = {
   color_scheme = 'Monokai',
   window_background_opacity = 0.9,
@@ -6,6 +7,28 @@ local config = {
   text_background_opacity = 0.9,
   window_decorations = 'NONE',
 }
+
+if wezterm.target_triple:find('windows') then
+  config.default_prog = { 'wsl.exe', '--cd', '~' }
+  config.launch_menu = {}
+  for _, dom in ipairs(wezterm.default_wsl_domains()) do
+    table.insert(config.launch_menu, {
+      label = dom.name,
+      args = { 'wsl.exe', '-d', dom.distribution, '--cd', '~' },
+      domain = { DomainName = 'local' },
+    })
+  end
+  table.insert(config.launch_menu, {
+    label = 'PowerShell',
+    args = { 'powershell.exe' },
+    domain = { DomainName = 'local' },
+  })
+end
+
+wezterm.on('gui-startup', function(cmd)
+  local _, _, window = wezterm.mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
 
 wezterm.on('format-tab-title', function(tab)
   local pane = tab.active_pane
@@ -15,13 +38,9 @@ wezterm.on('format-tab-title', function(tab)
   }
 end)
 
-if wezterm.target_triple:find('windows') then
-  config.default_domain = 'WSL:default~'
-end
-
-wezterm.on('gui-startup', function(cmd)
-  local _, _, window = wezterm.mux.spawn_window(cmd or {})
-  window:gui_window():maximize()
+wezterm.on('new-tab-button-click', function(window, pane)
+  window:perform_action(wezterm.action.ShowLauncherArgs { flags = 'FUZZY|LAUNCH_MENU_ITEMS' }, pane)
+  return false
 end)
 
 config.keys = {
@@ -35,6 +54,11 @@ config.keys = {
   },
   { key = 'W', mods = 'CTRL|SHIFT', action = wezterm.action.CloseCurrentTab { confirm = true } },
   { key = 'Q', mods = 'CTRL|SHIFT', action = wezterm.action.QuitApplication },
+  {
+    key = 'T',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.ShowLauncherArgs { flags = 'FUZZY|LAUNCH_MENU_ITEMS' },
+  },
 }
 
 return config
